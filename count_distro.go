03dfa530d3c4517feb32ccb5e74d32b66933cdf8
Chris Lane <chris@disputingtaste.com>
2024-01-31 18:43:48 +0000

    higher bucket resolutions

--- count_distro.go
+++ count_distro.go
@@ -6,12 +6,139 @@ package counters
 // like 1, 2, 5, 10, 20, 50, 100, 200, 500, 1K, 2K, 5K, ... etc.
 
 import (
+	"fmt"
 	"math"
 )
+
+type Resolution func (float64, int, string) string
+var theResolution = HighRes
 
 var units = []string{"f", "p", "n", "mi", "m", "", "k", "M", "G", "T", "P"}
 
 var unitSort = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+
+func LowRes(shortVal float64, size3 int, unit string) string {
+	s := ""
+	if shortVal >= 500 {
+		s = "500" + unit + "-1" + units[size3+6]
+	} else if shortVal >= 200 {
+		s = "200" + unit + "-500" + unit
+	} else if shortVal >= 100 {
+		s = "100" + unit + "-200" + unit
+	} else if shortVal >= 50 {
+		s = "050" + unit + "-100" + unit
+	} else if shortVal >= 20 {
+		s = "020" + unit + "-50" + unit
+	} else if shortVal >= 10 {
+		s = "010" + unit + "-20" + unit
+	} else if shortVal >= 5 {
+		s = "005" + unit + "-10" + unit
+	} else if shortVal >= 2 {
+		s = "002" + unit + "-5" + unit
+	} else if shortVal >= 1 {
+		s = "001" + unit + "-2" + unit
+	}
+
+	return s
+}
+
+func MediumRes(shortVal float64, size3 int, unit string) string {
+	s := ""
+
+	if shortVal >= 900 {
+		s = "900" + unit + "-1" + units[size3+6]
+	} else if shortVal >= 800 {
+		s = "800" + unit + "-900" + unit
+	} else if shortVal >= 700 {
+		s = "700" + unit + "-800" + unit
+	} else if shortVal >= 600 {
+		s = "600" + unit + "-700" + unit
+	}	else if shortVal >= 500 {
+		s = "500" + unit + "-600" + unit
+	} else if shortVal >= 400 {
+		s = "400" + unit + "-500" + unit
+	} else if shortVal >= 300 {
+		s = "300" + unit + "-400" + unit
+	} else if shortVal >= 200 {
+		s = "200" + unit + "-300" + unit
+	} else if shortVal >= 100 {
+		s = "100" + unit + "-200" + unit
+	} else if shortVal >= 90 {
+		s = "090" + unit + "-100" + unit
+	} else if shortVal >= 80 {
+		s = "080" + unit + "-90" + unit
+	} else if shortVal >= 70 {
+		s = "070" + unit + "-80" + unit
+	} else if shortVal >= 60 {
+		s = "060" + unit + "-70" + unit
+	} else if shortVal >= 50 {
+		s = "050" + unit + "-60" + unit
+	} else if shortVal >= 40 {
+		s = "040" + unit + "-50" + unit
+	} else if shortVal >= 30 {
+		s = "030" + unit + "-40" + unit
+	} else if shortVal >= 20 {
+		s = "020" + unit + "-30" + unit
+	} else if shortVal >= 10 {
+		s = "010" + unit + "-20" + unit
+	} else if shortVal >= 9 {
+		s = "009" + unit + "-10" + unit
+	} else if shortVal >= 8 {
+		s = "008" + unit + "-9" + unit
+	} else if shortVal >= 7 {
+		s = "007" + unit + "-8" + unit
+	} else if shortVal >= 6 {
+		s = "006" + unit + "-7" + unit
+	} else if shortVal >= 5 {
+		s = "005" + unit + "-6" + unit
+	} else if shortVal >= 4 {
+		s = "004" + unit + "-5" + unit
+	} else if shortVal >= 3 {
+		s = "003" + unit + "-4" + unit
+	} else if shortVal >= 2 {
+		s = "002" + unit + "-3" + unit
+	} else if shortVal >= 1 {
+		s = "001" + unit + "-2" + unit
+	}
+	return s
+}
+
+
+func HighRes(shortVal float64, size3 int, unit string) string {
+	s := ""
+
+	if shortVal >= 990 {
+		s = "990" + unit + "-1" + units[size3+6]
+		return s
+	}
+	if shortVal >= 99.99 {
+		for x := float64(980); x >= 100; x = x - 10 {
+			if shortVal >= x {
+				s = fmt.Sprintf("%03.0f", x) + unit + "-" + fmt.Sprintf("%.0f", x + 10) + unit
+				return s
+			}
+		}
+	}
+	if shortVal >= 9.99 {
+		for x := float64(99); x >= 10; x = x - 1.0 {
+			if shortVal >= x {
+				s = fmt.Sprintf("%03.0f", x) + unit + "-" + fmt.Sprintf("%.0f", x + 1) + unit
+				return s
+			}
+		}
+	}
+	for x := float64(9.9); x >= 1; x = x - 0.1 {
+		if shortVal >= x {
+			s = fmt.Sprintf("%05.1f", x) + unit + "-" + fmt.Sprintf("%.1f", x + 0.1) + unit
+			return s
+		}
+	}
+	return "err"
+}
+
+func SetResolution(f Resolution) {
+	theResolution = f
+}
 
 func deriveDistName(name string, value float64) string {
 
@@ -36,26 +163,7 @@ func deriveDistName(name string, value float64) string {
 	} else {
 		unit = "handleOddSizes(string, value)"
 	}
-	s := ""
-	if shortVal >= 500 {
-		s = "500" + unit + "-1" + units[size3+6]
-	} else if shortVal >= 200 {
-		s = "200" + unit + "-500" + unit
-	} else if shortVal >= 100 {
-		s = "100" + unit + "-200" + unit
-	} else if shortVal >= 50 {
-		s = "050" + unit + "-100" + unit
-	} else if shortVal >= 20 {
-		s = "020" + unit + "-50" + unit
-	} else if shortVal >= 10 {
-		s = "010" + unit + "-20" + unit
-	} else if shortVal >= 5 {
-		s = "005" + unit + "-10" + unit
-	} else if shortVal >= 2 {
-		s = "002" + unit + "-5" + unit
-	} else if shortVal >= 1 {
-		s = "001" + unit + "-2" + unit
-	}
+	s := theResolution(shortVal, size3, unit)
 	res = name + sign + unitOrder + "[" + s + "]"
 	return res
 }
--- count_distro_test.go
+++ count_distro_test.go
@@ -11,77 +11,126 @@ import (
 type t struct {
 	name    string
 	value   float64
-	derived string
+	lowResDerived string
+	mediumResDerived string
+	highResDerived string
 }
 
 var testsDerived = []t{
 	t{
 		name:    "test",
 		value:   1113.0,
-		derived: "testg[001k-2k]",
+		lowResDerived: "testg[001k-2k]",
+		mediumResDerived: "testg[001k-2k]",
+		highResDerived: "testg[001.1k-1.2k]",
 	},
 	t{
 		name:    "test",
 		value:   0.0,
-		derived: "test [zero]",
+		lowResDerived: "test [zero]",
+		mediumResDerived: "test [zero]",
+		highResDerived: "test [zero]",
 	},
 	t{
 		name:    "test",
 		value:   2113.0,
-		derived: "testg[002k-5k]",
+		lowResDerived: "testg[002k-5k]",
+		mediumResDerived: "testg[002k-3k]",
+		highResDerived: "testg[002.1k-2.2k]",
 	},
 	t{
 		name:    "test",
 		value:   5113.0,
-		derived: "testg[005k-10k]",
+		lowResDerived: "testg[005k-10k]",
+		mediumResDerived: "testg[005k-6k]",
+		highResDerived: "testg[005.1k-5.2k]",
 	},
 	t{
 		name:    "test",
 		value:   15113.0,
-		derived: "testg[010k-20k]",
+		lowResDerived: "testg[010k-20k]",
+		mediumResDerived: "testg[010k-20k]",
+		highResDerived: "testg[015k-16k]",
 	},
 	t{
 		name:    "test",
 		value:   45113.0,
-		derived: "testg[020k-50k]",
+		lowResDerived: "testg[020k-50k]",
+		mediumResDerived: "testg[040k-50k]",
+		highResDerived: "testg[045k-46k]",
 	},
 	t{
 		name:    "test",
 		value:   95113.0,
-		derived: "testg[050k-100k]",
+		lowResDerived: "testg[050k-100k]",
+		mediumResDerived: "testg[090k-100k]",
+		highResDerived: "testg[095k-96k]",
 	},
 	t{
 		name:    "test2",
 		value:   113.0,
-		derived: "test2f[100-200]",
+		lowResDerived: "test2f[100-200]",
+		mediumResDerived: "test2f[100-200]",
+		highResDerived: "test2f[110-120]",
 	},
 	t{
 		name:    "test2",
 		value:   213.0,
-		derived: "test2f[200-500]",
+		lowResDerived: "test2f[200-500]",
+		mediumResDerived: "test2f[200-300]",
+		highResDerived: "test2f[210-220]",
 	},
 	t{
 		name:    "test2",
 		value:   0.23,
-		derived: "test2e[200m-500m]",
+		lowResDerived: "test2e[200m-500m]",
+		mediumResDerived: "test2e[200m-300m]",
+		highResDerived: "test2e[230m-240m]",
 	},
 	t{
 		name:    "test2",
-		value:   0.83,
-		derived: "test2e[500m-1]",
+		value:   0.831,
+		lowResDerived: "test2e[500m-1]",
+		mediumResDerived: "test2e[800m-900m]",
+		highResDerived: "test2e[830m-840m]",
 	},
 	t{
 		name:    "test2",
 		value:   0.00083,
-		derived: "test2d[500mi-1m]",
+		lowResDerived: "test2d[500mi-1m]",
+		mediumResDerived: "test2d[800mi-900mi]",
+		highResDerived: "test2d[830mi-840mi]",
 	},
 }
 
-func TestDerivDistName(t *testing.T) {
+func TestDerivDistNameLow(t *testing.T) {
 	for _, te := range testsDerived {
+		SetResolution(LowRes)
 		s := deriveDistName(te.name, te.value)
-		if s != te.derived {
-			fmt.Println("Got", s, "Expected", te.derived, "from", te.value)
+		if s != te.lowResDerived {
+			fmt.Println("Got", s, "Expected", te.lowResDerived, "from", te.value)
+			t.Fail()
+		}
+	}
+}
+
+func TestDerivDistMedium(t *testing.T) {
+	for _, te := range testsDerived {
+		SetResolution(MediumRes)
+		s := deriveDistName(te.name, te.value)
+		if s != te.mediumResDerived {
+			fmt.Println("Got", s, "Expected", te.mediumResDerived, "from", te.value)
+			t.Fail()
+		}
+	}
+}
+
+func TestDerivDistNameHigh(t *testing.T) {
+	for _, te := range testsDerived {
+		SetResolution(HighRes)
+		s := deriveDistName(te.name, te.value)
+		if s != te.highResDerived {
+			fmt.Println("Got", s, "Expected", te.highResDerived, "from", te.value)
 			t.Fail()
 		}
 	}
